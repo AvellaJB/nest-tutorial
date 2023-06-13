f2466444dd106195a5af899d415b929fa525e61b
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2023-06-13 22:19:06 +0200

    custom guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
     getMe(@Request() req) {
         return { user: req.user}
